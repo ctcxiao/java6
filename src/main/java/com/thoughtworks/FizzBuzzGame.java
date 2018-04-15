1d62fec6740a8b3a06face8ad890c6e755574224
ctc <1171332103@qq.com>
2018-04-15 12:12:43 +0800

    提交完整代码

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -8,12 +8,54 @@ public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
 
     public void start(int count) {
-
+        for (int i = 1; i <= count; i++) {
+            System.out.println(dealWithNum(i));
+            results.add(dealWithNum(i));
+        }
     }
 
     public List<String> getResults() {
         return results;
     }
+
+    public String dealWithNum(int i) {
+
+        String result = String.valueOf(i);
+        if (result.contains("3")){
+            return "Fizz";
+        }
+
+        if (i % 5 == 0 && i % 7 == 0 && i % 3 == 0) {
+            return "FizzBuzzWhizz";
+        }
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        }
+
+        if (i % 3 == 0 && i % 7 == 0) {
+            return "FizzWhizz";
+        }
+
+        if (i % 5 == 0 && i % 7 == 0) {
+            return "BuzzWhizz";
+        }
+
+        if (i % 3 == 0) {
+            return "Fizz";
+        }
+
+        if (i % 5 == 0) {
+            return "Buzz";
+        }
+
+        if (i % 7 == 0) {
+            return "Whizz";
+        }
+
+        return result;
+    }
+    
 }
 
 
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,55 @@
 package com.thoughtworks;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class FizzBuzzGameTest {
     @Test
     public void should_return_Fizz_when_have_3() {
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        Assert.assertEquals("Fizz", fizzBuzzGame.dealWithNum(3));
+        Assert.assertNotEquals("Fizz", fizzBuzzGame.dealWithNum(5));
+    }
 
+    @Test
+    public void should_return_Buzz_when_have_5(){
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        Assert.assertEquals("Buzz", fizzBuzzGame.dealWithNum(5));
+        Assert.assertNotEquals("Buzz", fizzBuzzGame.dealWithNum(7));
+    }
+
+    @Test
+    public void should_return_Whizz_when_have_7(){
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        Assert.assertEquals("Whizz", fizzBuzzGame.dealWithNum(7));
+        Assert.assertNotEquals("Whizz", fizzBuzzGame.dealWithNum(12));
+    }
+
+    @Test
+    public void should_return_FizzBuzz_when_have_35(){
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        Assert.assertEquals("FizzBuzz", fizzBuzzGame.dealWithNum(15));
+        Assert.assertNotEquals("FizzBuzz", fizzBuzzGame.dealWithNum(35));
+    }
+
+    @Test
+    public void should_return_FizzWhizz_when_have_37(){
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        Assert.assertEquals("FizzWhizz", fizzBuzzGame.dealWithNum(21));
+        Assert.assertNotEquals("FizzWhizz", fizzBuzzGame.dealWithNum(37));
+    }
+
+    @Test
+    public void should_return_BuzzWhizz_when_have_57(){
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        Assert.assertEquals("BuzzWhizz", fizzBuzzGame.dealWithNum(70));
+        Assert.assertNotEquals("BuzzWhizz", fizzBuzzGame.dealWithNum(57));
+    }
+
+    @Test
+    public void should_return_FizzBuzzWhizz_when_have_357(){
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        Assert.assertEquals("FizzBuzzWhizz", fizzBuzzGame.dealWithNum(105));
+        Assert.assertNotEquals("FizzBuzzWhizz", 357);
     }
 }
